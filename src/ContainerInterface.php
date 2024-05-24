664530404f285a9566697594c4aa96e93e1f43bb
wheakerd <wheakerd@qq.com>
2024-05-24 19:11:54 +0800

    容器定义继承PSR11规范

---
+++ src/ContainerInterface.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace Skernl\Contract;
+
+use Psr\Container\ContainerInterface as PsrContainerInterface;
+
+interface ContainerInterface extends PsrContainerInterface
+{
+}
