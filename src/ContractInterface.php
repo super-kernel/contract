caaa0b3545a4e146950e646cd24788cad966b2fb
wheakerd <wheakerd@gmail.com>
2025-02-07 10:13:17 +0800

    feat: add a container interface

---
+++ src/ContractInterface.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace SuperKernel\Contract;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * @ContractInterface
+ * @\SuperKernel\Contract\ContractInterface
+ */
+interface ContractInterface extends ContainerInterface
+{
+}
