cd8d166b356e7f6723000c70aefc3d2481c9b3e6
wheakerd <wheakerd@gmail.com>
2025-05-09 21:06:04 +0800

    fix: Add container interface.

---
+++ src/ContainerInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SuperKernel\Contract;
+
+use Psr\Container\ContainerInterface as PsrContainerInterface;
+
+/**
+ * @see PsrContainerInterface
+ * @ContainerInterface
+ * @\SuperKernel\Contract\ContainerInterface
+ */
+interface ContainerInterface extends PsrContainerInterface
+{
+}
