17e3fa8a93edf70ed01c7eb3e10d009c17c9c3c8
wheakerd <wheakerd@gmail.com>
2025-05-10 11:39:08 +0800

    feat: Add config provider interface.

--- src/ConfigProviderInterface.php
+++ src/ConfigProviderInterface.php
@@ -4,10 +4,26 @@ declare(strict_types=1);
 namespace SuperKernel\Contract;
 
 /**
+ * Dependency libraries and configuration providers defined by developers must implement this interface.
+ * Warning: The implementation of this interface prohibits participation in operational interactions with containers.
+ *
  * @ConfigProviderInterface
  * @\SuperKernel\Contract\ConfigProviderInterface
  */
 interface ConfigProviderInterface
 {
-    public function __invoke(): array;
+	/**
+	 * Warning: Must be a public and parameter free constructor, the framework relies on this feature for automatic
+	 * instantiation.
+	 */
+	public function __construct();
+
+	/**
+	 * @return array{
+	 *     paths?: string[],        //  The directory path to be scanned.
+	 *     namespaces?: string[],   //  The namespace prefix to be scanned.
+	 *     classes?: string[],      //  The specified class name to be scanned.
+	 * }
+	 */
+	public function __invoke(): array;
 }
